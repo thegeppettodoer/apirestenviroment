43106ff02e87d6d5731b617eed751d5fd8c6af03
Ext.Victor.Garcia <ext.victor.garcia@sgs.com>
2022-07-13 13:55:40 -0500

    up ignore1

---
+++ src/index.js
@@ -0,0 +1,50 @@
+console.log("[Iniciando api rest para rimac]");
+
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// defining the Express app
+const app = express();
+// defining an array to work as the database (temporary solution)
+const ads = [
+  {title: 'Hello, world (again)!'}
+];
+
+// adding Helmet to enhance your Rest API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+
+var enviromentJson={
+  DEV:{
+    APIURL:"http:/www.aws.com/dev",
+  },
+  PROD:{
+    APIURL:"http:/www.aws.com/prov",
+  },
+};
+
+ 
+
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(enviromentJson);
+});
+ 
+// starting the server
+app.listen(3001, () => {
+  console.log('Escuchando en puerto 3001');
+});
